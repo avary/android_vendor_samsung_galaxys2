0e712c47403cb7d11db60e19b2dc7de96d79e28c
superatmel <superatmel@gmail.com>
2011-11-17 07:54:55 +0100

    Commit inicial ics

--- galaxys2-vendor-blobs.mk
+++ galaxys2-vendor-blobs.mk
@@ -14,52 +14,73 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \
-    vendor/samsung/galaxys2/proprietary/libril.so:obj/lib/libril.so \
-    vendor/samsung/galaxys2/proprietary/libsecril-client.so:obj/lib/libsecril-client.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudio.so:obj/lib/libaudio.so \
-    vendor/samsung/galaxys2/proprietary/audio/libmediayamahaservice.so:obj/lib/libmediayamahaservice.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudiopolicy.so:obj/lib/libaudiopolicy.so
-
-# All the blobs necessary for galaxys2
-
-# blobs necessary for audio
-PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxys2/proprietary/audio/alsa_amixer:system/bin/alsa_amixer \
-    vendor/samsung/galaxys2/proprietary/audio/alsa_aplay:system/bin/alsa_aplay \
-    vendor/samsung/galaxys2/proprietary/audio/alsa_ctl:system/bin/alsa_ctl \
-    vendor/samsung/galaxys2/proprietary/audio/alsa_ucm:system/bin/alsa_ucm \
-    vendor/samsung/galaxys2/proprietary/audio/libasound.so:system/lib/libasound.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudio.so:system/lib/libaudio.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudioeffect_jni.so:system/lib/libaudioeffect_jni.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudiohw_op.so:system/lib/libaudiohw_op.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudiohw_sf.so:system/lib/libaudiohw_sf.so \
-    vendor/samsung/galaxys2/proprietary/audio/libaudiopolicy.so:system/lib/libaudiopolicy.so \
-    vendor/samsung/galaxys2/proprietary/audio/liblvvefs.so:system/lib/liblvvefs.so \
-    vendor/samsung/galaxys2/proprietary/audio/libmediayamaha.so:system/lib/libmediayamaha.so \
-    vendor/samsung/galaxys2/proprietary/audio/libmediayamaha_jni.so:system/lib/libmediayamaha_jni.so \
-    vendor/samsung/galaxys2/proprietary/audio/libmediayamahaservice.so:system/lib/libmediayamahaservice.so \
-    vendor/samsung/galaxys2/proprietary/audio/libmediayamaha_tuning_jni.so:system/lib/libmediayamaha_tuning_jni.so \
-    vendor/samsung/galaxys2/proprietary/audio/libsamsungAcousticeq.so:system/lib/libsamsungAcousticeq.so \
-    vendor/samsung/galaxys2/proprietary/audio/lib_Samsung_Acoustic_Module_Llite.so:system/lib/lib_Samsung_Acoustic_Module_Llite.so \
-    vendor/samsung/galaxys2/proprietary/audio/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
-    vendor/samsung/galaxys2/proprietary/audio/libsamsungSoundbooster.so:system/lib/libsamsungSoundbooster.so \
-    vendor/samsung/galaxys2/proprietary/audio/lib_Samsung_Sound_Booster.so:system/lib/lib_Samsung_Sound_Booster.so \
-    vendor/samsung/galaxys2/proprietary/audio/libsoundalive.so:system/lib/libsoundalive.so \
-    vendor/samsung/galaxys2/proprietary/audio/libsoundpool.so:system/lib/libsoundpool.so \
-    vendor/samsung/galaxys2/proprietary/audio/libSR_AudioIn.so:system/lib/libSR_AudioIn.so \
-    vendor/samsung/galaxys2/proprietary/audio/libyamahasrc.so:system/lib/libyamahasrc.so
+    vendor/samsung/galaxys2/proprietary/ril/lib/libril.so:obj/lib/libril.so \
+    vendor/samsung/galaxys2/proprietary/ril/lib/libsecril-client.so:obj/lib/libsecril-client.so
 
 # RIL
 PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxys2/proprietary/libril.so:system/lib/libril.so \
-    vendor/samsung/galaxys2/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
-    vendor/samsung/galaxys2/proprietary/libsec-ril.so:system/lib/libsec-ril.so \
-    vendor/samsung/galaxys2/proprietary/rild:system/bin/rild
+    vendor/samsung/galaxys2/proprietary/ril/bin/rild:system/bin/rild \
+    vendor/samsung/galaxys2/proprietary/ril/lib/libril.so:system/lib/libril.so \
+    vendor/samsung/galaxys2/proprietary/ril/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/galaxys2/proprietary/ril/lib/libsecril-client.so:system/lib/libsecril-client.so
 
-# Keychars
+# EGL
 PRODUCT_COPY_FILES += \
-    vendor/samsung/galaxys2/proprietary/keychars/Broadcom_Bluetooth_HID.kcm.bin:system/usr/keychars/Broadcom_Bluetooth_HID.kcm.bin \
-    vendor/samsung/galaxys2/proprietary/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
-    vendor/samsung/galaxys2/proprietary/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
-    vendor/samsung/galaxys2/proprietary/keychars/sec_key.kcm.bin:system/usr/keychars/sec_key.kcm.bin \
-    vendor/samsung/galaxys2/proprietary/keychars/sec_touchkey.kcm.bin:system/usr/keychars/sec_touchkey.kcm.bin
+    vendor/samsung/galaxys2/proprietary/graphics/lib/libEGL.so:system/lib/libEGL.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/libGLESv1_CM.so:system/lib/libGLESv1_CM.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/libGLESv2.so:system/lib/libGLESv2.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/libMali.so:system/lib/libMali.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/libOpenSLES.so:system/lib/libOpenSLES.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/libUMP.so:system/lib/libUMP.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/egl/libEGL_mali.so:system/lib/egl/libEGL_mali.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/egl/libGLESv1_CM_mali.so:system/lib/egl/libGLESv1_CM_mali.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/egl/libGLESv2_mali.so:system/lib/egl/libGLESv2_mali.so \
+    vendor/samsung/galaxys2/proprietary/graphics/lib/hw/gralloc.smdkv310.so:system/lib/hw/gralloc.smdkv310.so
+
+# COMPOSER - needs tvout
+#PRODUCT_COPY_FILES += \
+#    vendor/samsung/galaxys2/proprietary/composer/lib/libfimg.so:system/lib/libfimg.so \
+#    vendor/samsung/galaxys2/proprietary/composer/lib/hw/hwcomposer.smdkv310.so:system/lib/hw/hwcomposer.smdkv310.so
+
+# AUDIO
+#PRODUCT_COPY_FILES += \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/asound.conf:system/etc/asound.conf \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/LVVEFS_Rx_Configuration.txt:system/etc/audio/LVVEFS_Rx_Configuration.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/LVVEFS_Tx_Configuration.txt:system/etc/audio/LVVEFS_Tx_Configuration.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Rx_ControlParams_BLUETOOTH_HEADSET.txt:system/etc/audio/Rx_ControlParams_BLUETOOTH_HEADSET.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Rx_ControlParams_EARPIECE_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_EARPIECE_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Rx_ControlParams_SPEAKER_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_SPEAKER_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Rx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Rx_ControlParams_WIRED_HEADSET_WIDEBAND.txt:system/etc/audio/Rx_ControlParams_WIRED_HEADSET_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Tx_ControlParams_BLUETOOTH_HEADSET.txt:system/etc/audio/Tx_ControlParams_BLUETOOTH_HEADSET.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Tx_ControlParams_EARPIECE_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_EARPIECE_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Tx_ControlParams_SPEAKER_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_SPEAKER_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Tx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_WIRED_HEADPHONE_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt:system/etc/audio/Tx_ControlParams_WIRED_HEADSET_WIDEBAND.txt \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libasound.so:system/lib/etc/audio/libasound.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libaudio.so:system/lib/etc/audio/libaudio.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/liblvvefs.so:system/lib/etc/audio/liblvvefs.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libmediayamaha.so:system/lib/etc/audio/libmediayamaha.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libmediayamaha_jni.so:system/lib/etc/audio/libmediayamaha_jni.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libmediayamahaservice.so:system/lib/etc/audio/libmediayamahaservice.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libsamsungAcousticeq.so:system/lib/etc/audio/libsamsungAcousticeq.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/lib_Samsung_Acoustic_Module_Llite.so:system/lib/etc/audio/lib_Samsung_Acoustic_Module_Llite.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libSamsungPDLComposer.so:system/lib/etc/audio/libSamsungPDLComposer.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/lib_Samsung_Resampler.so:system/lib/etc/audio/lib_Samsung_Resampler.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libsamsungSoundbooster.so:system/lib/etc/audio/libsamsungSoundbooster.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/lib_Samsung_Sound_Booster.so:system/lib/etc/audio/lib_Samsung_Sound_Booster.so \
+#    vendor/samsung/galaxys2/proprietary/audio/lib/libyamahasrc.so:system/lib/etc/audio/libyamahasrc.so
+
+# SENSORS
+PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxys2/proprietary/sensors/lib/libakm.so:system/lib/libakm.so
+
+# GPS
+PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxys2/proprietary/gps/lib/hw/gps.GT-I9100.so:system/lib/hw/gps.GT-I9100.so
+
+# WIFI
+PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxys2/proprietary/wifi/bin/BCM4330B1_002.001.003.0221.0265.hcd:system/bin/BCM4330B1_002.001.003.0221.0265.hcd
+
